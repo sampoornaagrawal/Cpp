a4a532892aca0f75bf5c6fbb8331a0f52b92a752
sampoornaagrawal <44267132+sampoornaagrawal@users.noreply.github.com>
2019-11-07 17:02:00 +0530

    decimal_precision.cpp
    using <iomanip> library setting the precision of decimal values.

---
+++ decimal_precision.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+   float pi=3.14159;
+   cout<<setprecision(3)<<pi<<endl; //including LHS of decimal (whole number)
+   cout<<fixed<<setprecision(3)<<pi; //after decimal
+}
